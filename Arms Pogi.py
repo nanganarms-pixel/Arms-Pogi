de4a816399be7989b9133b1b9f916a89999e83ab
nanganarms-pixel <nanganarms@gmail.com>
2026-01-30 12:15:45 +0800

    Improve number input validation and output
    Refactor input handling and fix syntax errors.

--- Arms Pogi.py
+++ Arms Pogi.py
@@ -1,7 +1,11 @@
-print("Enter a Number")
-x = int(input())
+  user_input = input("Enter a number: ")
+if user_input.lstrip("-").isdigit():
+  number = int(user_input)
 
-if x % 2 == 0
-print("Number is Even")
-else;
-print("Number is Odd)
+if x % 2 == 0:
+  print("Number is Even")
+else:
+  print("Number is Odd)
+        else:print("Invalid. Please Enter a Number")
+
+  
